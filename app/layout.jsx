fd5d238fecdd52640b5e9bbd8fa6a2384de8e202
Lythmass <lythmass17@gmail.com>
2023-12-06 17:57:05 +0400

    feat: change cursor

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { karla } from './fonts';
 import { Provider } from 'services';
 import { Frame, NavBar, ToggleTheme } from 'components';
 import { ToastContainer } from 'react-toastify';
+import AnimatedCursor from 'react-animated-cursor';
 
 export const metadata = {
   title: 'Gigi Lapachishvili',
@@ -13,7 +14,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`w-screen h-screen ${karla.className}`}>
+        <AnimatedCursor
+          color='255,255,255'
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={1.7}
+          outerStyle={{
+            mixBlendMode: 'exclusion',
+          }}
+        />
         <Provider>
+          {/* <Trail /> */}
           <ToastContainer
             position='top-left'
             autoClose={5000}
